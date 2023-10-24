558e4e3d6e034d8fad2ed76e350c634072c29c83
aliensbright <144503232+aliensbright@users.noreply.github.com>
2023-10-24 13:05:18 -0700

    Done

--- task1.py
+++ task1.py
@@ -1,6 +1,21 @@
+'''Create a program with 3 function definitions:
+function A prints the message "Hello"
+function B prints the message "How are you"
+function C prints the message "Hi"
+
+Ask the user to enter a letter from A to C
+Execute the function of the letter they use.
+'''
 def A():
-
+    print('Hello')
 def B():
-
-
+    print('How are you?')
 def C():
+    print('Hi')
+word=input('Enter a letter from A to C:')
+if word=='A':
+    A()
+elif word=='B':
+    B()
+elif word=='C':
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,23 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+guess=0
+num=random.randint(0,100)
+
+def title():
+    print('These are the rules for the game. You will need to guess a number.\nEnter a number from 0-100 and I will tell you if it is too low or high.')
+title()
+def game():
+    guess=int(input('Enter a number from 0-100:'))
+    if guess<num:
+        print('Too Low.')
+    elif guess>num:
+        print('Too High.')
+    elif guess==num:
+        print('You got it right')
+        exit()
+for i in range(10):
+    game()
+else:
+    print(f'You failed. The missing number was {num}.')
